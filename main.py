32aaa95e990a50cc689b568ceb0b9e8a86f33280
heckq <device8543@gmail.com>
2024-05-23 14:41:00 +0300

    added due date

--- main.py
+++ main.py
@@ -1,24 +1,33 @@
-from flask import Flask, render_template, url_for, request, redirect
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
+from datetime import datetime, timezone
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}@{os.getenv('POSTGRES_HOST')}/{os.getenv('POSTGRES_DB')}"
 db = SQLAlchemy(app)
 
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(200), nullable=False)
-    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+    date_created = db.Column(db.DateTime, default=lambda: datetime.now(timezone.utc))
+    due_date = db.Column(db.DateTime)
 
     def __repr__(self):
         return '<Task %r>' % self.id
 
-@app.route("/", methods = ['POST','GET'])
+@app.route("/", methods=['POST', 'GET'])
 def index():
     if request.method == 'POST':
         task_content = request.form['content']
-        new_task = Todo(content = task_content)
+        due_date_str = request.form['due_date']
+        if due_date_str:
+            due_date = datetime.strptime(due_date_str, '%Y-%m-%d')
+        else:
+            due_date = None
+        new_task = Todo(content=task_content, due_date=due_date)
         try:
             db.session.add(new_task)
             db.session.commit()
@@ -26,13 +35,12 @@ def index():
         except:
             return 'There was an issue adding your task'
     else:
-        tasks = Todo.query.order_by(Todo.date_created).all()
-        return render_template('index.html', tasks = tasks)
+        tasks = Todo.query.order_by(Todo.due_date).all()
+        return render_template('index.html', tasks=tasks)
 
 @app.route('/delete/<int:id>')
 def delete(id):
-    task_to_delete  = Todo.query.get_or_404(id)
-
+    task_to_delete = Todo.query.get_or_404(id)
     try:
         db.session.delete(task_to_delete)
         db.session.commit()
@@ -40,23 +48,26 @@ def delete(id):
     except:
         return 'There was a problem deleting that task'
 
-
-@app.route("/update/<int:id>", methods = ['POST','GET'])
+@app.route("/update/<int:id>", methods=['POST', 'GET'])
 def update(id):
     task = Todo.query.get_or_404(id)
-
     if request.method == 'POST':
         task.content = request.form['content']
-
+        due_date_str = request.form['due_date']
+        if due_date_str:
+            task.due_date = datetime.strptime(due_date_str, '%Y-%m-%d')
+        else:
+            task.due_date = None
         try:
             db.session.commit()
             return redirect('/')
         except:
             return 'There was an issue updating your task'
-
     else:
-        return render_template('update.html', task=task)
-
+        due_date_str = task.due_date.strftime('%Y-%m-%d') if task.due_date else ''
+        return render_template('update.html', task=task, due_date_str=due_date_str)
 
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
